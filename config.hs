9b3c747af232b46a957e3cafa1cc71985bd347c0
Anton Ekblad <anton@ekblad.cc>
2015-12-31 15:02:43 +0100

    Make update interval configurable.

--- config.hs
+++ config.hs
@@ -6,12 +6,12 @@ import Haste.Events
 import Server.API
 import System.IO.Unsafe
 
-newTextField :: String -> String -> Bool -> String -> Client (Elem, Client String)
-newTextField caption ident pass def = do
+newTextField :: String -> String -> String -> String -> Client (Elem, Client String)
+newTextField caption ident type_ def = do
   label <- newElem "label" `with` [ "textContent" =: caption
                                   , "for" =: ident
                                   , style "margin" =: "0.5em" ]
-  e <- newElem "input" `with` [ "type" =: if pass then "password" else "text"
+  e <- newElem "input" `with` [ "type" =: type_
                               , "id" =: ident
                               , "value" =: def
                               , style "margin" =: "0.5em"]
@@ -47,24 +47,34 @@ setOverlayMessage :: String -> Client ()
   overlay `onEvent` Click $ \_ -> deleteChild documentBody outer
   return (outer, setProp msg "textContent")
 
+-- | Save configuration with pretty confirmation screen.
 saveConfig :: API -> Config -> Client ()
 saveConfig api cfg = do
   appendChild documentBody saveOverlay
   onServer $ setCfg api <.> cfg
   setOverlayMessage "Configuration saved, click to continue"
 
+-- | Gelbooru configuration.
 booruConfigBox :: API -> Config -> Client Elem
 booruConfigBox api cfg@(Config {..}) = do
-  (user, getUser) <- newTextField "Gelbooru username:" "user" False cfgBooruUsername
-  (pass, getPass) <- newTextField "Gelbooru password:" "pass" True cfgBooruPassword
-  (tags, getTags) <- newTextField "Gelbooru tags:" "tags" False (unwords cfgBooruTags)
-  hdr <- newElem "h3" `with` [ "textContent" =: "Gelbooru configuration"
-                             , style "margin" =: "0.5em"]
-  btn <- newElem "button" `with` [ "textContent" =: "Save"
-                                 , style "margin" =: "0.5em"]
-  box <- newElem "div" `with` [ children [hdr, user, pass, tags, btn]
-                              , style "display" =: "inline-block"
-                              , style "border" =: "1px solid black"]
+  (user, getUser) <- newTextField "Gelbooru username:" "user" "text"
+                                  cfgBooruUsername
+  (pass, getPass) <- newTextField "Gelbooru password:" "pass" "password"
+                                  cfgBooruPassword
+  (tags, getTags) <- newTextField "Gelbooru tags:" "tags" "text"
+                                  (unwords cfgBooruTags)
+  hdr <- newElem "h3" `with`
+    [ "textContent" =: "Gelbooru configuration"
+    , style "margin" =: "0.5em"]
+  btn <- newElem "button" `with`
+    [ "textContent" =: "Save"
+    , style "margin" =: "0.5em"]
+  box <- newElem "div" `with`
+    [ children [hdr, user, pass, tags, btn]
+    , style "display" =: "inline-block"
+    , style "border" =: "1px solid black"
+    , style "width" =: "100%"
+    , style "margin" =: "1em"]
   btn `onEvent` Click $ \_ -> do
     u' <- getUser
     p' <- getPass
@@ -77,6 +87,29 @@ booruConfigBox api cfg@(Config {..}) = do
     saveConfig api cfg'
   return box
 
+-- | Display configuration.
+displayConfigBox :: API -> Config -> Client Elem
+displayConfigBox api cfg@(Config {..}) = do
+  (delay, getSecs) <- newTextField "Seconds between frames:" "s" "number"
+                                   (show cfgSlideDuration)
+  hdr <- newElem "h3" `with`
+    [ "textContent" =: "Display configuration"
+    , style "margin" =: "0.5em"]
+  btn <- newElem "button" `with`
+    [ "textContent" =: "Save"
+    , style "margin" =: "0.5em"]
+  box <- newElem "div" `with`
+    [ children [hdr, delay, btn]
+    , style "display" =: "inline-block"
+    , style "border" =: "1px solid black"
+    , style "width" =: "100%"
+    , style "margin" =: "1em"]
+  btn `onEvent` Click $ \_ -> do
+    s' <- getSecs
+    let cfg' = cfg {cfgSlideDuration = read s'}
+    saveConfig api cfg'
+  return box
+
 main :: IO ()
 main = runApp defaultConfig $ do
   api <- newAPI
@@ -84,4 +117,8 @@ main = runApp defaultConfig $ do
     set documentBody [style "font-family" =: "helvetica"]
     cfg <- onServer $ getCfg api
     booru <- booruConfigBox api cfg
-    setChildren documentBody [booru]
+    disp <- displayConfigBox api cfg
+    outer <- newElem "div" `with`
+      [ children [disp, booru]
+      , style "width" =: "30em"]
+    setChildren documentBody [outer]
